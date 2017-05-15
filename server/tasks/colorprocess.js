2c4670430d7b94465dd0dcf16377f50ac58551d1
kitajchuk <kitajchuk@gmail.com>
2017-05-15 10:33:32 -0700

    Color processing with 10 point source image sampling

--- server/tasks/colorprocess.js
+++ server/tasks/colorprocess.js
@@ -10,6 +10,8 @@ const core = {
 };
 const getColors = require( "get-image-colors" );
 const chroma = require( "chroma-js" );
+const slackbot = require( "./slackbot" );
+const cli = require( "cli" );
 
 
 
@@ -35,64 +37,111 @@ const getQueryColors = function ( colors ) {
 
 
 
-prismic.api( core.config.api.access, null ).then(( api ) => {
-    //console.log( api );
-    api.query( prismic.Predicates.at( "document.type", core.config.skylab.mainType ) ).then(( json ) => {
-        //console.log( json );
-        let images = [];
-        const results = [];
+const doColorProcess = function ( token, webhook, channel ) {
+    prismic.api( core.config.api.access, null ).then(( api ) => {
+        const message = [];
+        const context = "colorprocess-task";
 
-        // Iterate project documents and get ALL associated images
-        json.results.forEach(( doc ) => {
-            const slices = doc.getSliceZone( `${core.config.skylab.mainType}.slices` );
+        //console.log( api );
+        api.query( prismic.Predicates.at( "document.type", core.config.skylab.mainType ) ).then(( json ) => {
+            //console.log( json );
+            let images = [];
+            const results = [];
 
-            if ( slices ) {
-                slices.value.forEach(( slice ) => {
-                    if ( slice.sliceType === "image" ) {
-                        images.push({
-                            url: slice.value.url,
-                            doc: `/${doc.type}/${doc.uid}/`
-                        });
-                    }
-                });
-            }
-        });
+            // Iterate project documents and get ALL associated images
+            json.results.forEach(( doc ) => {
+                const slices = doc.getSliceZone( `${core.config.skylab.mainType}.slices` );
 
-        console.log( core.config.logger, `Image Color Processing for ${images.length} Images` );
-
-        // images = images.slice( 0, 6 );
-
-        // console.log( core.config.logger, `Splicing ${images.length} Images for Color Processing` );
-
-        // Iterate all associated images and get color sources
-        const getImageColors = function ( image ) {
-            getColors( image.url ).then(( colors ) => {
-                const result = {
-                    query: getQueryColors( colors ),
-                    image: image,
-                    colors: colors.map(( color ) => {
-                        return color.hex();
-                    })
-                };
-
-                console.log( core.config.logger, "Processed Image Colors", result.colors );
-                console.log( "" );
-
-                results.push( result );
-
-                if ( !images.length ) {
-                    console.log( core.config.logger, "Image Color Processing Done" );
-
-                    core.file.write( path.join( core.config.skylab.cacheDir, "colorprocess.json" ), JSON.stringify( results, null, 4 ) ).then(() => {
-                        console.log( core.config.logger, "Image Color Date Saved" );
+                if ( slices ) {
+                    slices.value.forEach(( slice ) => {
+                        if ( slice.sliceType === "image" ) {
+                            images.push({
+                                url: slice.value.url,
+                                doc: `/${doc.type}/${doc.uid}/`
+                            });
+                        }
                     });
-
-                } else {
-                    getImageColors( images.pop() );
                 }
             });
-        };
 
-        getImageColors( images.pop() );
+            console.log( core.config.logger, `Image Color Processing for ${images.length} Images` );
+                message.push( `Image Color Processing for ${images.length} Images` );
+
+
+            // Splice small Array for test runs...
+            // images = images.slice( 0, 6 );
+            // console.log( core.config.logger, `Splicing ${images.length} Images for Color Processing` );
+            //     message.push( `Splicing ${images.length} Images for Color Processing` );
+
+
+            // Iterate all associated images and get color sources
+            const getImageColors = function ( image ) {
+                getColors( image.url ).then(( colors ) => {
+                    const result = {
+                        query: getQueryColors( colors ),
+                        image: image,
+                        colors: colors.map(( color ) => {
+                            return color.hex();
+                        })
+                    };
+
+                    console.log( core.config.logger, "Processed Image Colors", result.colors );
+                        message.push( `Processed Image Colors ${result.colors.join( ", " )} — ${image.url}` );
+                    console.log( "" );
+
+                    results.push( result );
+
+                    if ( !images.length ) {
+                        console.log( core.config.logger, "Image Color Processing Done" );
+                            message.push( "Image Color Processing Done" );
+
+                        core.file.write( path.join( core.config.skylab.cacheDir, "colorprocess.json" ), JSON.stringify( results, null, 4 ) ).then(() => {
+                            console.log( core.config.logger, "Image Color Date Saved" );
+                                message.push( "Image Color Data Saved" );
+
+                            slackbot.ping({
+                                token,
+                                webhook,
+                                channel,
+                                message,
+                                context
+                            });
+                        });
+
+                    } else {
+                        getImageColors( images.pop() );
+                    }
+                });
+            };
+
+            getImageColors( images.pop() );
+        });
     });
+};
+
+
+
+cli.setApp( "colorprocess", "0.1.0" );
+
+
+
+cli.parse({
+    token: ["token", "The Slack app integration token.", "string", ""],
+    webhook: ["webhook", "The Slack app integration webhook URL.", "string", ""],
+    channel: ["channel", "The Slack channel to ping.", "string", ""]
 });
+
+
+
+if ( cli.options.token && cli.options.webhook && cli.options.channel ) {
+    doColorProcess(
+        cli.options.token,
+        cli.options.webhook,
+        cli.options.channel
+    );
+
+} else {
+    console.log( "Requires Slack token, webhook URL and channel to ping." );
+
+    process.exit( 1 );
+}
