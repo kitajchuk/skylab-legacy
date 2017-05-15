0e929ba046d578b3e8c61f2b7f0b35fe0e0670e6
kitajchuk <kitajchuk@gmail.com>
2017-05-15 16:42:37 -0700

    Fix assignment to const variable :P

--- server/core/content.js
+++ server/core/content.js
@@ -1,3 +1,7 @@
+"use strict";
+
+
+
 const path = require( "path" );
 const core = {
     watch: require( "./watch" ),
@@ -16,8 +20,8 @@ const ContextObject = require( "../class/ContextObject" );
  */
 const getPage = function ( req, res, listener ) {
     return new Promise(( resolve, reject ) => {
+        let context = new ContextObject( page );
         const page = (req.params.type ? req.params.type : core.config.homepage);
-        const context = new ContextObject( page );
         const check = function ( data ) {
             // 0.0 => Missing template file
             // 0.1 => Single ContentItem
