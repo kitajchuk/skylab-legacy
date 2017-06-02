bf94c8ef2a22d61cf6c0042fa59b615fb9bde764
kitajchuk <kitajchuk@gmail.com>
2017-06-01 19:08:22 -0700

    Fix bug in app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const router = require( "./router" );
 const shuffle = require( "shuffle-array" );
 const lager = require( "properjs-lager" );
 const request = require( "request" );
-const imageJSON = `${config.deply.cdnURL}/json/imageprocess.json`;
+const imageJSON = `${config.deploy.cdnURL}/json/imageprocess.json`;
 
 
 
