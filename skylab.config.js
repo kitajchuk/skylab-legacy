afa825cee1f0bb4347f7b943273ac4ef97192d90
kitajchuk <kitajchuk@gmail.com>
2017-06-29 16:55:52 -0700

    create root install script

--- skylab.config.js
+++ skylab.config.js
@@ -55,6 +55,7 @@ const config = {
 
     // Skylab-www specific ( Prismic )
     skylab: {
+        hoboBuild: "is eq not attr index filter detach remove append toggleClass",
         mainForm: "sitewide",
         naviFrag: "site.navi",
         siteType: "site",
