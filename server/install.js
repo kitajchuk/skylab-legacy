1ef8e33a14c85ebda58f2b897ccff84902add978
kitajchuk <kitajchuk@gmail.com>
2017-08-24 11:27:32 -0700

    update server install

--- server/install.js
+++ server/install.js
@@ -10,30 +10,29 @@ const child_process = require( "child_process" );
 // The ultimate resolve was to trash the `.npm` cache
 
 
-console.log( "Installing node_modules..." );
-
+console.log( `Installing node_modules...` );
 
 // 1.0: No `node_modules`
-if ( !fs.existsSync( rootNodeModules ) ) {
-    child_process.execSync( "npm install" );
-
-// 2.0 Reset `node_modules` cache
-} else {
-    child_process.execSync( `rm -rf ${rootNodeModules}` );
-    child_process.execSync( "npm install" );
-}
+child_process.execSync( `rm -rf ${rootNodeModules}` );
+child_process.execSync( "npm install" );
 
 
-console.log( "Removing package-lock.json..." );
+console.log( `Removing package-lock.json...` );
 
-// 3.0 Remove `package-lock.json` for now
+// 2.0 Remove `package-lock.json` for now
 child_process.execSync( `rm -rf ${rootPackageLock}` );
 
 
 console.log( `Stopping ${process.env.NODE_ENV} server...` );
 
-// 4.0 Stop `environment` server
+// 3.0 Stop `environment` server
 child_process.execSync( "npm run stop" );
+
+
+console.log( `Forwarding port 80 to port 8000...` );
+
+// 4.0 Make sure ports are forwarded for node
+child_process.execSync( "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000" );
 
 
 console.log( `Starting ${process.env.NODE_ENV} server...` );
