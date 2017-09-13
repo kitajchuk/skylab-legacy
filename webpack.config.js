b3f4bf0389d6e820def7ac7648d0730c4e729f45
kitajchuk <kitajchuk@gmail.com>
2017-09-12 21:59:21 -0700

    Fix font path in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const webpack = require( "webpack" );
 const autoprefixer = require( "autoprefixer" );
 const BrowserSyncPlugin = require( "browser-sync-webpack-plugin" );
 const CompressionPlugin = require( "compression-webpack-plugin" );
-const sassFontPath = (config.aws.cdnOn && !config.env.sandbox && !config.env.staging) ? `${config.aws.cdn}/fonts/` : "/fonts/";
+const sassFontPath = (config.aws.cdnOn && !config.env.sandbox) ? `${config.aws.cdn}/fonts/` : "/fonts/";
 
 
 
