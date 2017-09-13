ccd5e6c042bf12ba9c1c7fb179da178cfb72e166
kitajchuk <kitajchuk@gmail.com>
2017-09-12 21:36:07 -0700

    Fix bad config.deploy.cdnURL ref to be config.aws.cdn

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const router = require( "./router" );
 const shuffle = require( "shuffle-array" );
 const lager = require( "properjs-lager" );
 const request = require( "request" );
-const imageJSON = config.env.sandbox ? `http://localhost:${config.browser.port}/json/imageprocess.json` : `${config.deploy.cdnURL}/json/imageprocess.json`;
+const imageJSON = config.env.sandbox ? `http://localhost:${config.browser.port}/json/imageprocess.json` : `${config.aws.cdn}/json/imageprocess.json`;
 
 
 
