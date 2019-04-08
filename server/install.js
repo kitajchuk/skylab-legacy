341f404351a1bf7d39afb7644ebc46381ed1d750
kitajchuk <kitajchuk@gmail.com>
2019-04-08 09:53:29 -0700

    swith to PM2

--- server/install.js
+++ server/install.js
@@ -4,33 +4,40 @@ const root = __dirname;
 const rootNodeModules = path.join( root, "node_modules" );
 const rootPackageLock = path.join( root, "package-lock.json" );
 const child_process = require( "child_process" );
+const pidLogFile = path.join( root, "pid.log" );
+const config = require( "../skylab.config" );
 
 
 // Note that with `npm@5` there have been some hiccups
 // The ultimate resolve was to trash the `.npm` cache
 
 
-console.log( `Installing node_modules...` );
+// 1.0: Fresh `node_modules`
+console.log( "Installing node_modules..." );
 
-// 1.0: No `node_modules`
 child_process.execSync( `rm -rf ${rootPackageLock}` );
 // child_process.execSync( `rm -rf ${rootNodeModules}` );
 child_process.execSync( "npm install" );
 
 
-console.log( `Stopping ${process.env.NODE_ENV} server...` );
-
 // 2.0 Stop `environment` server
-child_process.execSync( "npm run stop" );
+if ( fs.existsSync( pidLogFile ) ) {
+    console.log( `Stopping ${process.env.NODE_ENV} server...` );
 
+    child_process.execSync( "npm run stop" );
 
-console.log( `Forwarding port 80 to port 8000...` );
+} else {
+    console.log( `No PID file, skipping stop command...` );
+}
+
 
 // 3.0 Make sure ports are forwarded for node
-child_process.execSync( "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000" );
+// console.log( `Forwarding port 80 to port 8000...` );
 
+// child_process.execSync( "iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000" );
 
-console.log( `Starting ${process.env.NODE_ENV} server...` );
 
 // 4.0 Start `environment` server
+console.log( `Starting ${process.env.NODE_ENV} server...` );
+
 child_process.execSync( `npm run start:${process.env.NODE_ENV}` );
